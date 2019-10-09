5ad13899c97d2b6b869d4eca38d2567a74537e71
Quentin Rimoux <quentin.rimoux@live.fr>
2019-10-09 16:44:02 -0400

    Initial work on a custom CountDownTimer class.

---
+++ ITTW/Classes/CountDownTimer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITTW.Classes
+{
+    /// <summary>
+    /// Encapsulates the System.Windows.Forms.Timer class to gives a way
+    /// to have countdown timers.
+    /// <see cref="https://stackoverflow.com/a/54119639/8462331"/>.
+    /// </summary>
+    public class CountDownTimer : IDisposable
+    {
+        // ATTRIBUTES
+
+        public Action TimeChanged { get; set; }
+
+        public Action CountDownFinished { get; set; }
+
+        private Timer _timer = new Timer();
+
+        public bool IsRunning => _timer.Enabled;
+
+        private DateTime _maxTime = new DateTime(1, 1, 1, 0, 30, 6);
+
+        private DateTime _minTime = new DateTime(1, 1, 1, 0, 0, 0);
+
+        public int TimerInterval
+        {
+            get => _timer.Interval;
+            set => _timer.Interval = value;
+        }
+
+        public DateTime TimeLeft { get; private set; }
+
+        private long TimeLeftMs => TimeLeft.Ticks / TimeSpan.TicksPerMillisecond;
+
+        public string TimeLeftString => TimeLeft.ToString("mm:ss");
+
+        public string TimeLeftMsString => TimeLeft.ToString("mm:ss.fff");
+
+        // CONSTRUCTORS
+
+        public CountDownTimer(int min, int sec)
+        {
+            SetTime(min, sec);
+            Init();
+        }
+
+        public CountDownTimer(DateTime dateTime)
+        {
+            SetTime(dateTime);
+            Init();
+;        }
+
+        // METHODS
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        public void Init()
+        {
+
+        }
+
+        public void SetTime(DateTime dateTime)
+        {
+            TimeLeft = _maxTime = dateTime;
+            TimeChanged?.Invoke();
+        }
+
+        public void SetTime(int min, int sec = 0) => SetTime(new DateTime(1, 1, 1, 0, min, sec));
+    }
+}
--- ITTW/ViewModels/MainWindowViewModel.cs
+++ ITTW/ViewModels/MainWindowViewModel.cs
@@ -17,40 +17,10 @@ namespace ITTW.ViewModels
             set => SetProperty(ref _title, value);
         }
 
-        private Timer _timer;
-
-        private int _counter = (int) Config.TimerSpan.TotalSeconds;
-
-        private DateTime _elapsedTime;
-
-        public DateTime ElapsedTime
-        {
-            get => _elapsedTime;
-            set => SetProperty(ref _elapsedTime, value, nameof(TextTimer));
-        }
-
-
-        public DateTime StartedTime { get; set; }
-
-        public DateTime TimeToReach { get; set; }
-
-        private TimeSpan _elapsedSpan;
-
-        public TimeSpan ElapsedSpan
-        {
-            get => _elapsedSpan;
-            set => SetProperty(ref _elapsedSpan, value, nameof(TextTimer));
-        }
-
         public string _textTimer;
         public string TextTimer
         {
-            get
-            {
-                string text; 
-                text = $"Remaining time : {ElapsedSpan.Hours:00}:{ElapsedSpan.Minutes:00}:{ElapsedSpan.Seconds:00}";
-                return text;
-            }
+            get => "Remaining time";
             set => _textTimer = value;
         }
 
@@ -58,32 +28,10 @@ namespace ITTW.ViewModels
 
         private void StartTimer()
         {
-            if (_timer == null)
-            {
-                _timer = new Timer();
-            }
-
-            _timer.Interval = 1000;
-            _timer.Elapsed += TimerOnElapsed;
-            StartedTime = DateTime.Now;
-            TimeToReach = DateTime.Now + Config.TimerSpan;
-            _timer.Start();
-        }
-
-        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
-        {
-            _counter--;
-            ElapsedSpan = ElapsedSpan.Subtract(new TimeSpan(0, 0, 1));
-            if (_counter == 0)
-            {
-                _timer.Stop();
-            }
         }
 
         public MainWindowViewModel()
         {
-            _timer = new Timer();
-            ElapsedSpan = Config.TimerSpan;
         }
     }
 }
